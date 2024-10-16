b88e2bf1ddd006e9d2893ef34a5aba48f3cff9cf
KopalaTommy <kopalatommy@gmail.com>
2024-10-16 10:06:49 -0600

    Added new print when configuring Qt for host build

--- RaspiQtCrossCompile_QtHostBuild.sh
+++ RaspiQtCrossCompile_QtHostBuild.sh
@@ -22,7 +22,9 @@ echo -e "${GREEN}Starting Qt Host Build${NC}"
 # Build Qt for host
 echo -e "${GREEN}Building Qt for Host${NC}"
 cd $BUILD_LOC/host-build/
+echo 'cmake $SOURCE_CACHE_LOC/qt-everywhere-src-${QtMajorVersion}.${QtMinorVersion}.${QtPatchVersion}/ -GNinja -DCMAKE_BUILD_TYPE=Release -DQT_BUILD_EXAMPLES=OFF -DQT_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${BUILD_LOC}/host -DCMAKE_CXX_COMPILER_LAUNCHER=ccache'
 cmake $SOURCE_CACHE_LOC/qt-everywhere-src-${QtMajorVersion}.${QtMinorVersion}.${QtPatchVersion}/ -GNinja -DCMAKE_BUILD_TYPE=Release -DQT_BUILD_EXAMPLES=OFF -DQT_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${BUILD_LOC}/host -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+
 
 # Exit if failed
 if [ $? -ne 0 ]; then
